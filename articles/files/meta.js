481e4928f955a965e42752cdc48ed47ac0355c7f
Arin Davoodian <arindavoodian@gmail.com>
2019-11-26 07:50:20 -0800

    folder name change, hightload images update

---
+++ articles/files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"5d5428ef38486b1a472d29ee5f4c5d63-3.html":"The idea of coding interviews disturbs me. Yes, companies need to access your abilities, but coding interviews invade your privacy. Just imagine, some","a73a0fd1cedcd6cf3632e7bd01bc3b59-2.html":"\tThis is just the initial post for my portfolio blog? I guess it is. Anyway just testing functionality before posting anything else. Although, this ma","archive-november-2019.html":"Archives for November 2019"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
